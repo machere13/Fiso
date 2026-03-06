e0ecf9f3c51ed946cf20d072487d3ab0d37c29a6
machere13 <vladislav.chubarov13@yandex.ru>
2026-03-06 12:30:54 +0300

    feat: added choose path func

--- app/ui/main_window.py
+++ app/ui/main_window.py
@@ -1,13 +1,28 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import filedialog, ttk
+
 
 class MainWindow(ttk.Frame):
     def __init__(self, master: tk.Misc, **kwargs) -> None:
         super().__init__(master, **kwargs)
         self._build_ui()
 
-    def _build_ui(self) -> None: ...
+    def _build_ui(self) -> None:
+        self.columnconfigure(1, weight=1)
 
-    def _on_browse(self) -> None: ...
+        # стилизуется здесь, через грид
+        ttk.Label(self, text="Путь").grid(row=0)
+
+        self._path_var = tk.StringVar()
+        # стилизуется здесь, через грид
+        ttk.Entry(self, textvariable=self._path_var).grid(row=0)
+        
+        # стилизуется здесь, через грид
+        ttk.Button(self, text="Обзор", command=self._on_browse).grid(row=0)
+
+    def _on_browse(self) -> None:
+        directory = filedialog.askdirectory()
+        if directory:
+            self._path_var.set(directory)
 
     def _on_sort(self) -> None: ...
